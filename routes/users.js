865d9a3544d960cc63e214071a7f41d3db6b183d
Tanatat Wongpanyanurak <boomnoobb@gmail.com>
2022-11-26 11:19:41 +0700

    workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello Kitty');
+  res.status(200).json({
+    fullname:'Tanatat Wongpunyanurak'
+  })
+});
+router.get('/bio', function(req, res, next) {
+  //res.send('Hello Kitty');
+  res.status(200).json({
+    fullname:'Tanatat Wongpunyanurak',
+    nickname:'Boom',
+    hobby:'play game and lisen music',
+    gitusername:'Buukunn'
+  })
 });
 
 module.exports = router;
